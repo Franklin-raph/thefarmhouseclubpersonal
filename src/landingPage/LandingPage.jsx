9df41a0e7d5f563500a6b3e365a1c85bd00f7f17
franklin-raph <igboekwulusifranklin@gmail.com>
2023-08-05 15:53:08 +0100

    update

--- src/landingPage/LandingPage.jsx
+++ src/landingPage/LandingPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import logo from "../assets/images/Asset-2.-300x47.png"
 import coin1 from "../assets/images/Coin1.png"
 import coin2 from "../assets/images/Coin2.png"
@@ -5,12 +6,37 @@ import { useNavigate } from "react-router-dom"
 
 const LandingPage = () => {
     const navigate = useNavigate()
+
+    
+    useEffect(() => {
+        const nav = document.querySelector("nav ul")
+        const navOpen = document.querySelector(".fa-bars")
+        const navClose = document.querySelector(".fa-xmark")
+        
+        navOpen.addEventListener("click", ()=>{
+            nav.style.left = 0
+            navClose.style.display = "block"
+            navOpen.style.display = "none"
+        })
+
+        navClose.addEventListener("click", ()=>{
+            nav.style.left = "-200%"
+            navClose.style.display = "none"
+            navOpen.style.display = "block"
+        })
+    },[])
+
+
   return (
     <div className='landingPage'>
         <nav className="flex items-center justify-between px-[16px] py-5">
             <a href="/" className="w-[12rem] flex items-center gap-1">
                 <img src={logo} alt="" />
             </a>
+            <div className="toggler relative z-50">
+                <i class="fa-solid fa-bars"></i>
+                <i class="fa-solid fa-xmark"></i>
+            </div>
             <ul className="flex items-center justify-between gap-[30px]">
                 <li>
                     <a href="#">Dashboard</a>
